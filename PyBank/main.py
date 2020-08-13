0e075a70fa5753e68c22ea366b8cb3a19c87578d
Evan Kamis <Kamishnr@gmail.com>
2020-08-12 21:33:27 -0400

    create textfile with results

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,9 +2,9 @@
 import csv
 import os
 
- # Path to collect data from the Resources folder
-open_file = os.path.join('PyBank', 'Resources', 'budget_data.csv')
-output_file = os.path.join('PyBank','analysis', 'report.txt')
+ # Path to collect data from the Resources folder and send to output text file
+open_file = os.path.join('Resources', 'budget_data.csv')
+output_file = os.path.join('analysis', 'report.txt')
 
 totalMonths = 0 
 totalProfitLoss= 0
@@ -21,11 +21,14 @@ with open(open_file, 'r') as csvfile:
   # Split the data on commas
   csvreader = csv.reader(csvfile, delimiter=',')
   #id first row as header
+  #header_row = next(csvreader)
   header_row = next(csvreader)
-  row = next(csvreader)
+  #row = next(csvreader)
 
   totalProfitLoss= 0
-  priorChange = int(row[1])
+  priorChange = 0
+  #priorChange = int(row[1])
+  
    
   #loop each row of data 
   for row in csvreader:
@@ -62,16 +65,21 @@ avgProfitChg = sum(monthlyChange) / len(monthlyChange)
   # The greatest decrease in losses (date and amount) over the entire period
 
 # Print out the Financial Analysis to terminal 
-analysis = (
-  print("Financial Analysis"),
-  print("-----------------------------------------------------------------"),
-  print(f"Total Months: {str(totalMonths)}"),
-  print(f"Total Profit/Loss: ${str(totalProfitLoss)}"),
-  print(f"Average Monthly Profit change: ${avgProfitChg:.2f}"),
-  print(f"Greatest Profit Increase: {str(greatestIncreaseMonthYr)} (${int(greatestIncrease)})"),
-  print(f"Greates Profit Decrease: {str(greatestDecreaseMonthYr)} (${int(greatestDecrease)})"))
+
+print("Financial Analysis"),
+print("-----------------------------------------------------------------"),
+print(f"Total Months: {str(totalMonths)}"),
+print(f"Total Profit/Loss: ${str(totalProfitLoss)}"),
+print(f"Average Monthly Profit change: ${avgProfitChg:.2f}"),
+print(f"Greatest Profit Increase: {str(greatestIncreaseMonthYr)} (${int(greatestIncrease)})"),
+print(f"Greates Profit Decrease: {str(greatestDecreaseMonthYr)} (${int(greatestDecrease)})")
 
 #send Analysis to output file
-f = open(output_file, "w")
-f.write(analysis)
-f.close
+with open (output_file, 'w') as textfile:
+  textfile.write("Financial Analysis\n"),
+  textfile.write("-----------------------------------------------------\n"),
+  textfile.write(f"Total Months: {str(totalMonths)}\n"),
+  textfile.write(f"Total Profit/Loss: ${str(totalProfitLoss)}\n"),
+  textfile.write(f"Average Monthly Profit change: ${avgProfitChg:.2f}\n"),
+  textfile.write(f"Greatest Profit Increase: {str(greatestIncreaseMonthYr)} (${int(greatestIncrease)})\n"),
+  textfile.write(f"Greates Profit Decrease: {str(greatestDecreaseMonthYr)} (${int(greatestDecrease)})\n")
